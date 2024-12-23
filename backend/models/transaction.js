0bed175fa0136452971f5003f71cfe9000a29b24
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-12-23 15:56:36 +0800

    feat(models): add Transaction model

---
+++ backend/models/transaction.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+
+const { sequelize } = require('../utils/db');
+
+class Transaction extends Model {};
+
+Transaction.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  joNumber: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+    defaultValue: 1,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  remarks: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: true,
+  modelName: 'transaction'
+});
+
+module.exports = Transaction;
