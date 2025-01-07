2d364cde3606843b042525d98bb567fefeec7335
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2025-01-07 11:52:40 +0800

    feat: create axios routes for backend API access

---
+++ frontend/src/services/clientService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/api/clients';
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+const getOne = async (id) => {
+  const response = await axios.get(`${baseUrl}/${id}`)
+  return response.data
+}
+
+const create = async (newClient) => {
+  const response = await axios.post(baseUrl, newClient);
+  return response.data;
+};
+
+export default {
+  getAll,
+  getOne,
+  create,
+};
