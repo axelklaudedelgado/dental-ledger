e6a4d622bfc37ef65c75ae6253a0137dce9ffff0
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2025-02-20 14:56:33 +0800

    feat: add axios call for creating a transaction

--- frontend/src/services/transactionService.js
+++ frontend/src/services/transactionService.js
@@ -2,9 +2,14 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/api/transactions'
 
+const createTransaction = async (newTransaction) => {
+	const response = await axios.post(baseUrl, newTransaction)
+	return response.data
+}
+
 const nextJONumber = async () => {
 	const response = await axios.get(`${baseUrl}/next-jo-number`)
 	return response.data
 }
 
-export default { nextJONumber }
+export default { createTransaction, nextJONumber }
