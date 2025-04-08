36a2f5c0f487e84d96e0d41fccd3f8f4c908bf9c
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2025-04-08 22:30:10 +0800

    feat: add getName as a prop when decoding slug

--- frontend/src/utils/decodeClientSlug.js
+++ frontend/src/utils/decodeClientSlug.js
@@ -1,10 +1,17 @@
-const decodeClientSlug = (slug) => {
+const decodeClientSlug = (slug, getName = false) => {
 	const parts = slug.split('-')
 	const encodedId = parts.pop()
+	const capitalizedParts = parts.map(
+		(part) => part.charAt(0).toUpperCase() + part.slice(1),
+	)
 	const id = parseInt(encodedId, 36)
 
 	if (isNaN(id)) {
 		throw new Error('Invalid slug format')
+	}
+
+	if (getName) {
+		return capitalizedParts.join(' ')
 	}
 
 	return id
