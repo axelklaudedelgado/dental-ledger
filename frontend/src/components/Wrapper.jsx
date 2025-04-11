297d0094284441332245394275b596da802c999b
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2025-04-11 21:59:18 +0800

    style: adjust wrapper padding depending on screen

--- frontend/src/components/Wrapper.jsx
+++ frontend/src/components/Wrapper.jsx
@@ -162,7 +162,7 @@ function Wrapper() {
 	}
 
 	return (
-		<div className="container mx-auto py-6 max-w-7xl px-4">
+		<div className="container mx-auto py-6 max-w-7xl px-4 md:px-6 lg:px-8">
 			<header>{renderHeader()}</header>
 			{renderBreadcrumbs()}
 			<main>
