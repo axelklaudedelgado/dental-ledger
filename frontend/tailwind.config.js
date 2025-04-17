650ac3771a437e68d99482ef8291191919c0f0d9
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2025-04-17 13:26:24 +0800

    feat: add accordion component for use in mobile

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,6 +17,28 @@ export default {
 					foreground: 'hsl(var(--scn-muted-foreground))',
 				},
 			},
+			keyframes: {
+				'accordion-down': {
+					from: {
+						height: '0',
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)',
+					},
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)',
+					},
+					to: {
+						height: '0',
+					},
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
 		},
 	},
 	plugins: [tailwindcssAnimate],
